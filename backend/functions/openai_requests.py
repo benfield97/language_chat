cabb1478a0bc9acaab792fc970ba2129d973752a
Ben Field <fieldben97@gmail.com>
2024-01-21 20:24:18 +1100

    added get chat response function

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -7,8 +7,10 @@ OPENAI_API_KEY = config('OPENAI_API_KEY')
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
+#import custom functions
+from functions.database import get_recent_messages
 
-#conveert audio to transcription
+#conveert audio to text
 def convert_audio_to_text(audio_file):
     try: 
         transcript = client.audio.transcriptions.create(
@@ -21,3 +23,28 @@ def convert_audio_to_text(audio_file):
     except Exception as e:
         print(e)
         return 
+    
+# openai chatgpt
+#get response to message
+def get_chat_response(message_input):
+    client = OpenAI()
+
+    messages = get_recent_messages()
+    user_message = {'role': 'user',
+                    
+                    'content': message_input}
+    messages.append(user_message)
+    print(messages)
+
+    try:
+        response = client.chat.completions.create(
+            model='gpt-4',
+            messages=messages
+                                                  )
+        message = response.choices[0].message
+
+        return message
+    except Exception as e:
+        print(e)
+        return
+
